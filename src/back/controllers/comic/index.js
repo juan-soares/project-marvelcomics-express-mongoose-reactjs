30247764d339d492328228e8ef2eaeeb8c186be3
Juan Carlos <juan.css013@gmail.com>
2022-07-06 22:59:19 -0300

    added comic data

--- src/back/controllers/comic/index.js
+++ src/back/controllers/comic/index.js
@@ -14,7 +14,84 @@ async function getComic(req, res) {
 }
 
 async function postComic(req, res) {
-  const newComic = new Comic({
+  
+  
+  // ADD-BY-LOTS
+  const dates = [
+    "1979-10-16",
+    "1979-11-20",
+    "1979-12-18",
+    "1980-01-15",
+    "1980-02-19",
+    "1980-03-18",
+    "1980-04-15",
+    "1980-05-20",
+    "1980-06-17",
+    "1980-07-15",
+    "1980-08-19",
+    "1980-09-16",
+    "1980-10-21",
+    "1980-11-18",
+    "1980-16-12",
+    "1981-01-13",
+    "1981-02-17",
+    "1981-03-17",
+    "1981-04-14",
+    "1981-05-12",
+    "1981-06-09",
+    "1981-07-14",
+    "1981-08-11",
+    "1981-09-08",
+    "1981-10-13",
+    "1981-11-10",
+    "1981-08-12",
+    "1982-01-12",
+    "1982-02-09",
+    "1982-03-09",
+    "1982-04-06",
+    "1982-05-04",
+    "1982-06-08",
+    "1982-07-06",
+    "1982-08-10",
+    "1982-09-07",
+    "1982-10-12",
+    "1982-11-09",
+    "1982-12-07",
+    "1983-01-11",
+    "1983-02-08",
+    "1983-03-08",
+    "1983-04-05"
+
+  ];
+  let n = "243";
+
+  dates.map(async (date) => {
+    let c = new Comic({
+      date: date,
+      nameUsa: `Incredible Hulk Vol 1 #${n}`,
+      nameBra: "?",
+      vol: "Incredible Hulk Vol 1",
+      downloaded: false,
+      readen: false,
+      translated: false,
+      earths: ["62a5421acc8a92fac50099af"],
+      newCharacters: [],
+      stories: "?",
+      arc: "?",
+      cover: `/images/covers/Incredible_Hulk_Vol_1_${n}.jpg`,
+    });
+
+    n++;
+
+    await c.save((err) => {
+      // eslint-disable-next-line no-undef
+      if (err) handleError(err);
+    });
+  });
+
+
+  // ADD-BY-ONE
+  /* const newComic = new Comic({
     date: req.body.date,
     nameUsa: req.body.nameUsa,
     nameBra: req.body.nameBra,
@@ -38,7 +115,7 @@ async function postComic(req, res) {
   await newComic.save((err) => {
     // eslint-disable-next-line no-undef
     if (err) handleError(err);
-  });
+  }); */
 
   res.status(200).json(messages.sucessSave);
 }
