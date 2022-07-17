785df671cd3334e560be0da80ab3a3d917a36055
Juan Carlos <juan.css013@gmail.com>
2022-07-17 17:09:21 -0300

    added comic data

--- src/back/controllers/comic/index.js
+++ src/back/controllers/comic/index.js
@@ -16,70 +16,34 @@ async function getComic(req, res) {
 async function postComic(req, res) {
   // ADD-BY-LOTS
   const dates = [
-    "2019-11-06",
-    "2019-11-27",
-    "2019-12-11",
-    "2019-12-18",
-    "2020-01-08",
-    "2020-01-29",
-    "2020-02-19",
-    "2020-02-26",
-    "2020-03-11",
-    "2020-06-10",
-    "2020-07-22",
-    "2020-09-02",
-    "2020-10-14",
-    "2020-12-16",
-    "2021-01-27",
-    "2021-02-24",
-    "2021-04-28",
-    "2021-05-26",
-    "2021-06-16",
-    "2021-07-21",
-    "2021-09-01",
-    "2021-10-06",
-    "2021-12-01",
-    "2022-02-09",
-    "2022-05-18",
-    "2022-06-22",
-    "2022-07-13"
+   "2022-07-13"   
   ]
    
   let n = 1;
 
   dates.map(async (date) => {
     let c = new Comic({
-      vol: "New Mutants Vol 4",
-      cover: `/images/covers/New_Mutants_Vol_4_${n}.jpg`,
+      vol: "X-Men: Hellfire Gala Vol 1",
+      cover: `/images/covers/X-Men_Hellfire_Gala_Vol_1_${n}.jpg`,
       //cover: "/images/covers/no_cover.jpg",
       //irrelevant: true,
       downloaded: true,
       translated: false,
-      nameBra: "Novos Mutantes v4",
+      nameBra: "?",
       date: date,
       readen: false,
       earths: ["62a5421acc8a92fac50099af"],
       newCharacters: [],
       stories: "?",
-      arc: "?",
+      arc: "Destiny of X",
     });
 
     c.nameUsa = `${c.vol} #${n}`;
 
-    if (n < 16) c.translated = true;
-    if (n === 25) c.arc = "Destiny of X";
-    if (n === 20) c.arc = "Hellfire Gala";
-    if (n === 19) c.arc = "Hellfire Gala";
-    if (n === 14) c.arc = "Reign of X";
-    if (n === 13) c.arc = "X of Swords";
-    if (n === 6) c.arc = "Dawn of X";
-    if (n === 5) c.arc = "Dawn of X";
-    if (n === 4) c.arc = "Dawn of X";
-    if (n === 3) c.arc = "Dawn of X";
-    if (n === 2) c.arc = "Dawn of X";
-    if (n === 1) c.arc = "Dawn of X";
-
+  // if (n < 11) c.translated = true;
+  // if (n === 33) c.arc = "X-Men: Green";
     
+   
     console.log(c);
     n++;
 
